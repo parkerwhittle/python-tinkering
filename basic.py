1ea5037e7ba583a030706bc10cbfbb6487c3b7f4
parkerwhittle <parkerwhittle@gmail.com>
2017-08-05 21:27:37 -0700

    intermezzo....

--- basic.py
+++ basic.py
@@ -1,5 +1,10 @@
-def doSomething():
-    pass
+def do_something_else(value):
+    return "Foobar " + value
+
+
+def do_something():
+    print do_something_else(value="with vigor! Quite vigorously!")
+
 
 if __name__ == '__main__':
-    doSomething()
+    do_something()
